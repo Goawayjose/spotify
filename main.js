505472d09cbbcba1fea780de26643e0882606614
Jose Ramirez Imac <Jrramirez1994@gmail.com>
2017-03-21 22:05:46 -0700

    randomized bubbles

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ var app = new Vue({
   data: {
     songs: {
       song1: {
-        id: 1,
+        id: '100',
         title: 'Famous',
         artist: 'Kanye West',
+        cover: 'img/pablo.jpg',
         trackMp3: 'tunes/Famous.mp3'
       },
       song2: {
-        id: 2,
+        id: '40',
         title: 'Let it Happen',
         artist: 'Tame Impala',
+        cover: 'img/currents.jpg',
+        trackMp3: 'tunes/Let_It_Happen.mp3'
+      },
+      song3: {
+        id: '400',
+        title: 'Let it Happen',
+        artist: 'Tame Impala',
+        cover: 'img/currents.jpg',
         trackMp3: 'tunes/Let_It_Happen.mp3'
       }
     },
@@ -22,8 +31,32 @@ var app = new Vue({
 });
 
 
+var $newdiv = $('.circle-btn-xsSmall');
+var $newdivSiblings = $('.circle-btn-xsSmall').parent().siblings().children();
 
 
-console.log(app.$data.songs);
+
+
+var arr = document.getElementsByClassName('circle-btn-xsSmall');
+
+
+
+
+              console.log(arr);
+              console.log(arr[0]);
+              arr = [].slice.call($newdiv); 
+              arr.forEach(function(v,i,a) {
+                var posx = (Math.random() * ($(document).width())).toFixed();
+                var posy = (Math.random() * ($(document).height())).toFixed();
+
+                $newdiv.css({
+                    'position':'absolute',
+                    'left':posx+'px',
+                    'top':posy+'px'
+                  });
+                  console.log(posx);
+                  console.log(posy);
+              });
+
 
 console.log(app.$data.song);
